59e61b87bae0530e8361068d0f0a6812d51eee65
Benjamin Jackson <benjacksonk@gmail.com>
2023-05-12 13:51:41 -0400

    added ui and reading from file

--- src/main/java/org/yup/cardealership/DealershipFileManager.java
+++ src/main/java/org/yup/cardealership/DealershipFileManager.java
@@ -1,5 +1,9 @@
 package org.yup.cardealership;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DealershipFileManager {
     /*
     responsible for reading the dealership file, parsing the data,
@@ -7,4 +11,53 @@ public class DealershipFileManager {
     Also responsible for saving a dealership and the vehicles
     back into the file in the same pipe-delimited format
      */
+    public Dealership getDealership() {
+        try {
+            FileReader fileReader = new FileReader("inventory.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String fileLine = bufferedReader.readLine();
+
+            String[] headerParts = fileLine.split("\\|");
+
+            String dealerName = headerParts[0];
+            String dealerAddress = headerParts[1];
+            String dealerPhone = headerParts[2];
+
+            Dealership dealer = new Dealership(dealerName, dealerAddress, dealerPhone);
+
+            while((fileLine = bufferedReader.readLine()) != null) {
+
+                String[] vehicleLineParts = fileLine.split("\\|");
+
+                int vehicleVin = Integer.parseInt(vehicleLineParts[0]);
+                int vehicleYear = Integer.parseInt(vehicleLineParts[1]);
+                String vehicleMake = vehicleLineParts[2];
+                String vehicleModel = vehicleLineParts[3];
+                String vehicleType = vehicleLineParts[4];
+                String vehicleColor = vehicleLineParts[5];
+                int vehicleOdometer = Integer.parseInt(vehicleLineParts[6]);
+                double vehiclePrice = Double.parseDouble(vehicleLineParts[7]);
+
+                Vehicle vehicle = new Vehicle(vehicleVin, vehicleYear,
+                        vehicleMake, vehicleModel, vehicleType,
+                        vehicleColor, vehicleOdometer, vehiclePrice);
+
+                dealer.addVehicle(vehicle);
+            }
+
+            bufferedReader.close();
+            fileReader.close();
+
+            return dealer;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void saveDealership(Dealership dealership) {
+
+    }
 }
--- src/main/java/org/yup/cardealership/UserInterface.java
+++ src/main/java/org/yup/cardealership/UserInterface.java
@@ -1,5 +1,8 @@
 package org.yup.cardealership;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class UserInterface {
     /*
     responsible for all output to the screen, reading of user input,
@@ -8,4 +11,85 @@ public class UserInterface {
     UserInterface would call the appropriate Dealership method
     and then display the vehicles it returns.)
      */
+    private Dealership dealership;
+
+    public UserInterface() {
+
+    }
+
+    private void init() {
+        this.dealership = new DealershipFileManager().getDealership();
+    }
+
+    private void displayMenu() {
+        System.out.println("MENU");
+        System.out.println("1) Show All Vehicles");
+        System.out.println("else) Exit");
+    }
+
+    private void displayVehicles(ArrayList<Vehicle> vehicles) {
+        for (Vehicle vehicle: vehicles) {
+            System.out.printf("%s %s %s%n", vehicle.getYear(), vehicle.getMake(), vehicle.getModel());
+        }
+    }
+
+    public void display() {
+        init();
+
+        Scanner inputScanner = new Scanner(System.in);
+
+        boolean loop = true;
+        do {
+            displayMenu();
+
+            //read user command
+            String userInput = inputScanner.nextLine();
+
+            //process user request
+            switch (userInput) {
+                case "1" -> processGetAllVehiclesRequest();
+                default  -> loop = false;
+            }
+        } while (loop);
+    }
+
+    public void processGetByPriceRequest() {
+
+    }
+
+    public void processGetByMakeModelRequest() {
+
+    }
+
+    public void processGetByYearRequest() {
+
+    }
+
+    public void processGetByColorRequest() {
+
+    }
+
+    public void processGetByMileageRequest() {
+
+    }
+
+    public void processGetByVehicleTypeRequest() {
+
+    }
+
+    public void processGetAllVehiclesRequest() {
+        displayVehicles(dealership.getAllVehicles());
+    }
+
+    public void processAddVehicleRequest() {
+
+    }
+
+    public void processRemoveVehicleRequest() {
+
+    }
+
+    public Dealership getDealership() {
+        return dealership;
+    }
 }
